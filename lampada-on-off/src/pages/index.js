30cb9e0dc8acf61a6b30c0a4abe6307b5cc8a3ee
Oruam <96359831+jose1u2u3u4u5@users.noreply.github.com>
2024-02-21 22:14:14 -0300

    colocar som

--- lampada-on-off/src/pages/index.js
+++ lampada-on-off/src/pages/index.js
@@ -1,14 +1,52 @@
+import Image from "next/image";
+import { useState } from "react";
+import useSound from "use-sound";
+
+
 
 
 export default function Home() {
-  return (
+  const [estadoInterruptor, setEstadoInterruptor] = useState (false);
+  const [contador, setContador] = useState(0)
+  
+  
 
-    <div className="container-fluid">
-      <div className="row">
+ function mudarLampada(){
+  setEstadoInterruptor (!estadoInterruptor)
+  if(estadoInterruptor == false)
+  {
+  setContador(contador +1)
+  }
+ }
+ if (contador == 2)
+ {
+  return  <Image src={"/assets/boom.jpg"} width={286} height={388}/>
+ } if(contador ==2)
+ {
+   boom
+ }
+
+  return (
+      <div>
+
+        <h2>
+          {
+            contador > 2
+          ? `Você já ligou ${contador} vez${contador==2?"": "es"} a lâmpada`
+          : "Sua lâmpada queimou"
+          }
+        </h2>
+        {
+          estadoInterruptor == true
+        ? <Image src={"/assets/acesa.png"} width={286} height={388}/>
+        : <Image src={"/assets/apagada.png"} width={286} height={388}/>
         
+        
+        }
+        <button onClick={mudarLampada}>Iterruptor</button>
       </div>
 
-    </div>
+    
       
   );
 }
